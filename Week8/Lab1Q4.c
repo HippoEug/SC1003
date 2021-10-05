365cb634e7a7cddb8d64baee1b972c01293f9da1
Eugene <efoo10@gmail.com>
2021-10-05 23:57:42 +0800

    Create Lab1Q4.c

---
+++ Week8/Lab1Q4.c
@@ -0,0 +1,25 @@
+// Program calculates exponential
+
+#include <stdio.h>
+
+int main() {
+	float inputX;
+	double numerator = 1;
+	double denominator = 1;
+	double result = 1;
+
+	printf("Enter x: \n");
+	scanf_s("%f", &inputX);
+
+	for (int i = 1; i <= 10; i++) {
+    // Numerator: x^4 is x multiplied by x^3
+		numerator = numerator*inputX;
+    // Denominator: Factorial 4! is 4 multiplied by factorial 3!
+		denominator = denominator * i;
+
+		result += (numerator / denominator);
+		// Do not have to reinit numerator or denominator to 1 after every loop
+	}
+
+	printf("Result: %0.2f", result);
+}

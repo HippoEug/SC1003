89e9d7b0272d2b6c36933bdaeebf80c7c89af04c
Eugene <efoo10@gmail.com>
2021-10-05 20:39:20 +0800

    Create Lab1Q3.c

---
+++ Week8/Lab1Q3.c
@@ -0,0 +1,32 @@
+// Program prints a triangular pattern
+
+#include <stdio.h>
+
+int main() {
+	int height;
+
+	printf("Enter the height: \n");
+	scanf_s("%d", &height);
+	printf("Pattern: \n");
+
+	for (int i = 1; i <= height; i++) {
+		for (int repeat = 1; repeat <= i; repeat++) {
+			switch (i%3) {
+				case 0:
+					printf("3");
+					break;
+				case 1:
+					printf("1");
+					break;
+				case 2:
+					printf("2");
+					break;
+			}
+			
+			//if(height % 2 == 1) { // change something here
+			//	printf("%d", repeat);
+			//}
+		}
+		printf("\n");
+	}
+}

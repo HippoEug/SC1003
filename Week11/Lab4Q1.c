6da2d8ca0975ea3998b26094798481adead14f7c
Eugene <efoo10@gmail.com>
2021-10-25 04:30:30 +0800

    Create Lab4Q1.c

---
+++ Week11/Lab4Q1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+
+char *sweepSpace1(char *str);
+char *sweepSpace2(char *str);
+
+int main() {
+	char str[80], str2[80], *p;
+
+	printf("Enter the string: \n");
+	fgets(str, 80, stdin);
+	if (p = strchr(str, '\n')) {
+		*p = '\0';
+	}
+
+	strcpy(str2, str);
+
+	printf("sweepSpace1(): %s\n", sweepSpace1(str));
+	printf("sweepSpace2(): %s\n", sweepSpace2(str2));
+}
+
+// Function removes blank spaces using array notation
+char *sweepSpace1(char *str) {
+	int j = 0;
+
+	int stringLength = strlen(str);
+
+	for (int i = 0; i < stringLength; i++) {
+		if (str[i] != ' ') {
+			str[j] = str[i];
+			j++;
+		}
+	}
+	str[j] = '\0';
+	return str;
+}
+
+// Function removes blank spaces using pointer notation 
+char *sweepSpace2(char *str) {
+	int j = 0;
+
+	int stringLength = strlen(str);
+
+	for (int i = 0; i < stringLength; i++) {
+		if (*(str + i) != ' ') {
+			*(str + j) = *(str + i);
+			j++;
+		}
+	}
+	*(str + j) = '\0';
+	return str;
+}
